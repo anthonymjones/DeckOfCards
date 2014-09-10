0657927953f2c5058274d8f05fb6afecb9b36137
anthonymjones <anthony.mjones@gmail.com>
2014-09-10 16:02:46 -0600

    V1

---
+++ DeckOfCards/DeckOfCards/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //delcare new deck
+            Deck forPoker = new Deck();
+            //shuffle the cards
+            forPoker.Shuffle();
+            //deal cards
+            List<Cards> ListofCards = new List<Cards>();
+            ListofCards = forPoker.Deal(5);
+            Console.WriteLine("Dealt Cards: ");
+            foreach (var item in ListofCards)
+            {
+                item.DealFromDeck();
+            }
+
+            Console.WriteLine("\n\nRemaining Cards in the Deck");
+            forPoker.PrintDeck();
+
+
+            //Console.WriteLine(string.Join("\n", ListofCards.OrderBy(x=>x)));
+
+
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
